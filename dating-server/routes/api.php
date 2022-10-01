d174750cc2759230c4a6fb23dd6f1d316e13d4ee
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 02:21:00 +0300

    backend: middleware routing added

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,4 +20,10 @@ Route::post("/user/find", [UserController::class, "findUser"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/profile', [AuthController::class, 'profile']);
 });
