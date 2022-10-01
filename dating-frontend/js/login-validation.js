b61d80c0bace213ec9e67965853cfe511ad4ec0a
Mohamad Nassar <mnassar57@gmail.com>
2022-10-01 18:33:44 +0300

    frontend: login button event listener added

---
+++ dating-frontend/js/login-validation.js
@@ -0,0 +1,7 @@
+const login_email = document.getElementById("login-email");
+const login_password = document.getElementById("login-password");
+const login_btn = document.getElementById("login-btn");
+
+login_btn.addEventListener("click", () => {
+    console.log("Works");
+})
