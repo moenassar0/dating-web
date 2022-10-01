5ccd862cecd9a0dc17e5c9c612a32bd914c3c968
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 02:15:16 +0300

    frontend: refactored Functions js file

---
+++ dating-frontend/js/Functions.js
@@ -0,0 +1,50 @@
+/*export const js_functions = () => {
+    const js_functions = {};
+
+    js_functions.baseURL = "http://127.0.0.1:8000/api/auth";
+    
+    
+    js_functions.getAPI = async (api_url) => {
+        try{
+            return await axios(api_url);
+        }catch(error){
+            console.log("Error from GET api: ", error);
+        }
+    }
+    
+    js_functions.postAPI = async (api_url, api_data, api_token = null) => {
+        try{
+            return await axios.post(
+                api_url,
+                api_data,
+                { headers:{
+                        'Authorization' : "token " + api_token
+                    }
+                }
+            );
+        }catch(error){
+            console.log("Error from GET api: ", error);
+        }
+    }
+}*/
+
+export const baseURL = "http://127.0.0.1:8000/api/auth/login";
+export const postAPI = async (api_url, api_data, api_token = null) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data,
+            { headers:{
+                    'Authorization' : "token " + api_token
+                }
+            }
+        );
+    }catch(error){
+        console.log("Error from POST api: ", error);
+    }
+}
+
+export const consoleLog = () => {
+    console.log("gg");
+}
+
