fb5db6ba2ca81e58d1d8a370f51dc6499613f402
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 02:19:10 +0300

    backend: user model implemented with jwt token

--- dating-server/app/Models/User.php
+++ dating-server/app/Models/User.php
@@ -2,10 +2,35 @@
 
 namespace App\Models;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
+    use HasFactory, Notifiable, HasApiTokens;
+
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+    */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+    */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
