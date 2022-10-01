72c157584654ad42a79ff819b19fd56663405ddd
Mohamad Nassar <mnassar57@gmail.com>
2022-10-01 22:01:51 +0300

    frontend: login validation implemented

--- dating-frontend/js/login-validation.js
+++ dating-frontend/js/login-validation.js
@@ -2,6 +2,17 @@ const login_email = document.getElementById("login-email");
 const login_password = document.getElementById("login-password");
 const login_btn = document.getElementById("login-btn");
 
-login_btn.addEventListener("click", () => {
-    console.log("Works");
+login_btn.addEventListener("click", async () => {
+    try{
+        const response = await axios.post(
+            "http://127.0.0.1:8000/api/user/find",
+            {"email": login_email.value,
+             "password": login_password.value}
+             
+        );
+        console.log(response);
+
+    }catch(error){
+        console.log("Error from POST API", error);
+    }
 })
