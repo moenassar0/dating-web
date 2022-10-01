8d42fbff9bb391f691f918fe145939a24582bede
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 02:17:24 +0300

    backend: authenticate controlled base implemented

---
+++ dating-server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use Validator;
+use App\Models\User;
+class AuthController extends Controller
+{
+    public function _construct(){
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'f_name' => 'required',
+            'email' => 'required|string|unique:users',
+            'password' => 'required',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        
+        $user = new User;
+
+        $user->f_name = $request->f_name;
+        $user->l_name = $request->f_name;
+        $user->picture_url = $request->f_name;
+        $user->gender = $request->f_name;
+        $user->interested_gender = $request->f_name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+        return response()->json(['message' => 'success']);
+    }
+
+    /*public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        if(!$token=auth()->attempt($validator->validated())){
+            return response()->json(['error' => 'unauthroized'], 401);
+        }
+        return $this->createNewToken($token);
+    }
+
+    public function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL()*60,
+            'user' => auth()->user()
+        ]);
+    }*/
+
+    //////
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function profile(){
+        if(auth()->user()){
+            return response()->json(auth()->user());
+        }
+        else
+            echo 'gg';
+    }
+}
