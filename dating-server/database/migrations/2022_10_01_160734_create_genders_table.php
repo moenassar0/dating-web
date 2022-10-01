aa324a5ab26b496b0bcaba1d21207f51cf9194b0
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 02:20:35 +0300

    backend: migrates for user and gender added

---
+++ dating-server/database/migrations/2022_10_01_160734_create_genders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGendersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gender_options', function (Blueprint $table) {
+            $table->string('gender_option')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gender_options');
+    }
+}
