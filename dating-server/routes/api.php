bc49ce8ee3bf5a1f63d9c2dbd891b5e1e4457cf9
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 05:44:21 +0300

    backend: added tester function to find users

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -17,6 +17,7 @@ use App\Http\Controllers\AuthController;
 
 Route::post("/user/add", [UserController::class, "store"]);
 Route::post("/user/find", [UserController::class, "findUser"]);
+Route::post("/users/find", [UserController::class, "getUsers"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
