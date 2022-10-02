1b429cec98a1db79cad2bcd9acb96dfa7249df21
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 05:46:21 +0300

    frontend: changed login validation tester function

--- dating-frontend/js/login-validation.js
+++ dating-frontend/js/login-validation.js
@@ -1,4 +1,4 @@
-import * as Functions from "./js-functions.js";
+import * as Functions from "./Functions.js";
 
 const login_email = document.getElementById("login-email");
 const login_password = document.getElementById("login-password");
@@ -13,10 +13,13 @@ const login_btn = document.getElementById("login-btn");
 login_btn.addEventListener("click", async () => {
     const response = await Functions.postAPI(Functions.baseURL, {"email": login_email.value,
     "password": login_password.value})
-    console.log(response);
+    logUserIn(response.data);
 
 })
 
 async function logUserIn(data){
-    console.log(data);
+    if(data.access_token){
+        localStorage.setItem("token", data.access_token);
+        window.location.href="home.html";
+    }
 }
