a7dfeedfc8543c6641c4a6fd748d551052dd5c00
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 05:45:49 +0300

    backend: jwt token authentication implemented

--- dating-server/app/Http/Controllers/AuthController.php
+++ dating-server/app/Http/Controllers/AuthController.php
@@ -34,5 +34,96 @@ class AuthController extends Controller
         return response()->json(['message' => 'success']);
     }
 
+        /*public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string',
+            'password' => 'required',
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+        if(!$token=auth()->attempt($validator->validated())){
+            return response()->json(['error' => 'unauthroized'], 401);
+        }
+        return $this->createNewToken($token);
+    }
+
+    public function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL()*60,
+            'user' => auth()->user()
+        ]);
+    }*/
+
+    //////
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function profile(){
+        if(auth()->user()){
+            return response()->json(auth()->user());
+        }
+        else
+            echo 'gg';
+    }
+
 
 }
