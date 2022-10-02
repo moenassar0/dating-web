0e4d658bcc7c37631171a8b5dad7e8373b0072c8
Mohamad Nassar <mnassar57@gmail.com>
2022-10-02 19:52:28 +0300

    frontend: container populated with users

--- dating-frontend/js/home.js
+++ dating-frontend/js/home.js
@@ -1,13 +1,20 @@
 import * as Functions from "./Functions.js";
+import {ProfileCard} from "./Components/ProfileCard.js";
 
 let token = localStorage.getItem("token");
+const profile_card_container = document.getElementById("profile-card-container");
 
 
 //Find people the user is interested in
 async function getUsersData(){
     
     const response = await Functions.postAPI(Functions.baseURL + "/users/find", {}, token);
-    console.log(response);
+    
+    let profileCardHTML = '';
+    response.data.message.map(user => {
+        profileCardHTML += ProfileCard(user);
+    })
+    profile_card_container.innerHTML = profileCardHTML;
 }
 
 getUsersData();
