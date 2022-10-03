8dead2ac9ea53d6670f4b473b601e75aefbbc9ca
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 04:01:27 +0300

    frontend: favorite user function implemented

--- dating-frontend/js/FavoriteUser.js
+++ dating-frontend/js/FavoriteUser.js
@@ -1,3 +1,7 @@
-export const FavoriteUser = (id) => {
-    console.log(id);
+import * as Functions from "./Functions.js"; 
+
+export const FavoriteUser = async (id, token) => {
+    const url = "/user/favorite";
+    const response = await Functions.postAPI(Functions.baseURL + url, {favorited_user_id: id}, token)
+    console.log(response);
 }
