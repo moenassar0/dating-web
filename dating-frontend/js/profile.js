2317442e3bac7e5016d63ed0a359249fa5309969
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 16:08:59 +0300

    frontend: populated profile from database response

--- dating-frontend/js/profile.js
+++ dating-frontend/js/profile.js
@@ -1,5 +1,9 @@
-import * as Functions from "./Functions.js"; 
+import * as Functions from "./Functions.js";
+import {ProfilePage} from "./Components/ProfilePage.js";
+
 let token = localStorage.getItem("token");
+
+const profile_container = document.getElementById("profile-card-container");
 
 //Validate user's token, if valid refresh with a new token and a new expiration date
 async function validateToken(){
@@ -14,7 +18,10 @@ await validateToken();
 async function getProfile(){
     const response = await Functions.postAPI(Functions.baseURL + "/user/profile", {}, token);
     console.log(response);
-}
+    let profileHTML = '';
+    profileHTML += ProfilePage(response.data.message);
+    profile_container.innerHTML = profileHTML;
+};
 
 getProfile();
 
