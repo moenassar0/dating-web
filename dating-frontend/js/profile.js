c9b458edb1622c9f07523124435be76e78fb96fb
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 15:17:41 +0300

    frontend: profile info receieved

---
+++ dating-frontend/js/profile.js
@@ -0,0 +1,20 @@
+import * as Functions from "./Functions.js"; 
+let token = localStorage.getItem("token");
+
+//Validate user's token, if valid refresh with a new token and a new expiration date
+async function validateToken(){
+    const response = await Functions.postAPI(Functions.baseURL + "/auth/authUser", {}, token);
+    console.log(response);
+    localStorage.setItem("token", (response.data.access_token));
+    token = localStorage.getItem("token");
+}
+
+await validateToken();
+
+async function getProfile(){
+    const response = await Functions.postAPI(Functions.baseURL + "/user/profile", {}, token);
+    console.log(response);
+}
+
+getProfile();
+
