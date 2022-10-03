0931844fab1bff9b6ff36cb3fc0f54a8f2846a4b
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 15:18:18 +0300

    frontend: blocking added and token validation

--- dating-frontend/js/home.js
+++ dating-frontend/js/home.js
@@ -1,8 +1,18 @@
 import * as Functions from "./Functions.js";
 import {ProfileCard} from "./Components/ProfileCard.js";
 import {FavoriteUser} from "./FavoriteUser.js";
+import { BlockUser } from "./BlockUser.js";
 
 let token = localStorage.getItem("token");
+//Validate user's token, if valid refresh with a new token and a new expiration date
+async function validateToken(){
+    const response = await Functions.postAPI(Functions.baseURL + "/auth/authUser", {}, token);
+    localStorage.setItem("token", (response.data.access_token));
+    token = localStorage.getItem("token");
+}
+
+await validateToken();
+
 const profile_card_container = document.getElementById("profile-card-container");
 
 //Navigation buttons
@@ -15,6 +25,7 @@ const messages_btn = document.getElementById("messages-btn");
 const close_block_popup = document.getElementById("close-btn");
 const block_popup = document.getElementById("block-popup");
 const cover = document.getElementById("cover");
+const block_button = document.getElementById("block-button");
 close_block_popup.addEventListener("click", () => {
     block_popup.classList.add("hidden");
     cover.classList.add("hidden");
@@ -44,6 +55,11 @@ async function getUsersData(){
             action.children[1].addEventListener("click", () => {
                 block_popup.classList.remove("hidden");
                 cover.classList.remove("hidden");
+                block_button.addEventListener("click", () => {
+                    BlockUser(id, token);
+                    block_popup.classList.add("hidden");
+                    cover.classList.add("hidden");
+                })
             })
         });
     }
@@ -68,11 +84,4 @@ messages_btn.addEventListener("click", () => {
     window.location.href="messages.html";
 })
 
-//Validate user's token, if valid refresh with a new token and a new expiration date
-async function validateToken(){
-    const response = await Functions.postAPI(Functions.baseURL + "/auth/authUser", {}, token);
-    localStorage.setItem("token", (response.data.access_token));
-    token = localStorage.getItem("token");
-}
 
-validateToken();
