eac42de57e3b66339e3500ecbf338d462d1014f3
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 19:55:09 +0300

    frontend: refactor and added close btn function

--- dating-frontend/js/profile.js
+++ dating-frontend/js/profile.js
@@ -16,6 +16,8 @@ const edit_button = document.getElementById("edit-button");
 const close_popup_button = document.getElementById("close-btn");
 const cover = document.getElementById("cover");
 let error_message = "";
+const success_div = document.getElementById("success-div");
+const error_div = document.getElementById("error-div");
 
 async function editPopup(){
     const popup_edit_button = document.getElementById("edit-profile");
@@ -25,31 +27,33 @@ async function editPopup(){
     })
 
     edit_button.addEventListener("click", () => {
-        console.log(f_name.value, l_name.value, gender.options[gender.selectedIndex].value,
-            interested_gender.options[interested_gender.selectedIndex].value);
-    
-            error_message = "";
-            if(!validateName(f_name.value, 2)){
-                 error_message += "First name is too short! (Minimum 2 characaters) <br>"
-            }
-            if(!validateName(l_name.value, 2)){
-                error_message += "Last name is too short! (Minimum 2 characaters) <br>"
-            }
-            if(!validateName(bio.value, 12)){
-                error_message += "Biography is too short! (Minimum 12 characaters) <br>"
-            }
-            if(error_message != ''){
-                //success_div.classList.add("hidden");
-                //error_div.classList.remove("hidden");
-                //error_div.innerHTML = error_message;
-            }
-            else{
-                //error_div.classList.add("hidden");
-                //error_message = '';
-                //success_div.innerHTML = "Successfully sent the message!"
-                //success_div.classList.remove("hidden");
-            }
-            console.log(error_message);
+
+        error_message = "";
+        if(!validateName(f_name.value, 2)){
+                error_message += "First name is too short! (Minimum 2 characaters) <br>"
+        }
+        if(!validateName(l_name.value, 2)){
+            error_message += "Last name is too short! (Minimum 2 characaters) <br>"
+        }
+        if(!validateName(bio.value, 12)){
+            error_message += "Biography is too short! (Minimum 12 characaters) <br>"
+        }
+        if(error_message != ''){
+            success_div.classList.add("hidden");
+            error_div.classList.remove("hidden");
+            error_div.innerHTML = error_message;
+        }
+        else{
+            error_div.classList.add("hidden");
+            error_message = '';
+            success_div.innerHTML = "Updated profile!"
+            success_div.classList.remove("hidden");
+        }
+    })
+
+    close_popup_button.addEventListener("click", () => {
+        edit_form.classList.add("hidden");
+        cover.classList.add("hidden");
     })
 }
 
