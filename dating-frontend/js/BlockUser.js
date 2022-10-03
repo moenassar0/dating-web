a10ab68004a20ffe2f9df944c1c4e6b45d9a9595
Mohamad Nassar <mnassar57@gmail.com>
2022-10-03 13:53:35 +0300

    frontend: blocked user function added

---
+++ dating-frontend/js/BlockUser.js
@@ -0,0 +1,7 @@
+import * as Functions from "./Functions.js"; 
+
+export const BlockUser = async (id, token) => {
+    const url = "/user/block";
+    const response = await Functions.postAPI(Functions.baseURL + url, {blocked_user_id: id}, token)
+    console.log(response);
+}
