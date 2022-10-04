7964cf71e441e6510d533a46e209dfe91971e3f7
Mohamad Nassar <mnassar57@gmail.com>
2022-10-04 03:00:37 +0300

    backend: message controller added

---
+++ dating-server/app/Http/Controllers/MessageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Models\User;
+use App\Models\BlockedUser;
+use App\Models\Favorite;
+use App\Http\Controllers\DB;
+
+class MessageController extends Controller
+{
+    public function sendMessage(Request $request){
+        $message = new Message;
+        $id = auth()->user()->id;
+        $message->sender_id = $id;
+        $message->receiver_id = $request->receiver_id;
+        $message->message_content = $request->message_content;
+    }
+}
