28473a8e9e3db6fce500d05d7b82e68eac64a93e
Mohamad Nassar <mnassar57@gmail.com>
2022-10-04 06:22:35 +0300

    frontend: updated messenger picture

--- dating-frontend/js/Components/Messenger.js
+++ dating-frontend/js/Components/Messenger.js
@@ -2,7 +2,7 @@ export const Messenger = (messenger) => {
     return(
         `
             <div id = "${messenger.id}" class="chat-user">
-                <img class="circle-img" src="./assets/images/empty_profile.png">
+                <img class="circle-img" src="${messenger.picture_url}">
                 <span>${messenger.f_name + " " + messenger.l_name}</span>
             </div>
         `
