6adbcdc179495e09662703851aadf2d44ec2d043
Mohamad Nassar <mnassar57@gmail.com>
2022-10-04 03:12:15 +0300

    frontend: messages base implemented

---
+++ dating-frontend/js/messages.js
@@ -0,0 +1,17 @@
+import * as Functions from "./Functions.js"; 
+import {Messenger} from "./Components/Messenger.js";
+
+let token = localStorage.getItem("token");
+const chat_users = document.getElementById("chat-users");
+
+//Validate user's token, if valid refresh with a new token and a new expiration date
+async function validateToken(){
+    const response = await Functions.postAPI(Functions.baseURL + "/auth/authUser", {}, token);
+    localStorage.setItem("token", (response.data.access_token));
+    token = localStorage.getItem("token");
+}
+
+await validateToken();
+
+
+Functions.navigationButtons();
