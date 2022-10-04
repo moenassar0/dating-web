2f33fb2ffc7c48c5a2a02942a3c277ee49f3c133
Mohamad Nassar <mnassar57@gmail.com>
2022-10-04 19:32:18 +0300

    backend: cleaned up authcontroller code

--- dating-server/app/Http/Controllers/AuthController.php
+++ dating-server/app/Http/Controllers/AuthController.php
@@ -8,32 +8,6 @@ use Validator;
 use App\Models\User;
 class AuthController extends Controller
 {
-    public function _construct(){
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
-    }
-    public function register(Request $request){
-        $validator = Validator::make($request->all(), [
-            'f_name' => 'required',
-            'email' => 'required|string|unique:users',
-            'password' => 'required',
-        ]);
-        if($validator->fails()){
-            return response()->json($validator->errors()->toJson(), 400);
-        }
-        
-        $user = new User;
-
-        $user->f_name = $request->f_name;
-        $user->l_name = $request->l_name;
-        $user->picture_url = $request->f_name;
-        $user->gender = $request->gender;
-        $user->interested_gender = $request->interested_gender;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-        $user->save();
-        return response()->json(['message' => 'success']);
-    }
-
     public function login()
     {
         $credentials = request(['email', 'password']);
