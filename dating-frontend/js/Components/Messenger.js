f58d7810830713ddb940a6b5195727433cf4ae17
Mohamad Nassar <mnassar57@gmail.com>
2022-10-04 03:12:28 +0300

    frontend: basic messenger component added

---
+++ dating-frontend/js/Components/Messenger.js
@@ -0,0 +1,10 @@
+export const Messenger = (messenger) => {
+    return(
+        `
+            <div id = "" class="chat-user">
+                <img class="circle-img" src="./assets/images/empty_profile.png">
+                <span>Mohamad Nassar</span>
+            </div>
+        `
+    )
+}
