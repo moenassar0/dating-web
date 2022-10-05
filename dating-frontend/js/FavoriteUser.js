8b328546257917415a735d43109b7026a4b5eee5
Mohamad Nassar <mnassar57@gmail.com>
2022-10-05 20:18:03 +0300

    frontend: code cleanup

--- dating-frontend/js/FavoriteUser.js
+++ dating-frontend/js/FavoriteUser.js
@@ -3,5 +3,4 @@ import * as Functions from "./Functions.js";
 export const FavoriteUser = async (id, token) => {
     const url = "/user/favorite";
     const response = await Functions.postAPI(Functions.baseURL + url, {favorited_user_id: id}, token)
-    console.log(response);
 }
